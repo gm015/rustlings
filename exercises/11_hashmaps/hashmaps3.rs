f350b50f04fcf439f63363d50d17c27a672391b3
pdol <79060962+gm015@users.noreply.github.com>
2024-02-01 21:50:59 +0100

    hefty hashmap

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -14,11 +14,11 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
+#[derive(Debug)]
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
@@ -34,12 +34,35 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+        // solution:
+
+        // this fn below fails as it does not update values for the team_1_score
+        // of team_1
+        //scores.entry(team_1_name).or_insert( Team {goals_scored: team_1_score, goals_conceded: team_2_score});
+
+        // this works since there is not duplicated country from fn input
+        //scores.entry(team_2_name).or_insert( Team {goals_scored: team_2_score, goals_conceded: team_1_score});
+
+        // found (simple) solution which works
+        let team_1 = scores.entry(team_1_name).or_insert(Team {
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        team_1.goals_scored += team_1_score;
+        team_1.goals_conceded += team_2_score;
+
+        let team_2 = scores.entry(team_2_name).or_insert(Team {
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        team_2.goals_scored += team_2_score;
+        team_2.goals_conceded += team_1_score;
+
     }
+    //println!("values: {:?}\n",scores);
+    //for (k,v) in scores.iter() {
+    //    println!("values: {:?}\n",scores);
+    //}
     scores
 }
 
